7b79f4296a4d7e771c724f8a5ed40f456ab7b574
Soyeong Kim <kimchuorok@gmail.com>
2023-10-04 19:54:09 +0900

    exponential naming issue fixed

--- main_ini.py
+++ main_ini.py
@@ -27,31 +27,32 @@ def quadInteger(k, N, d):
 d = 2.0 # dimension
 
 # non-fixed param
-arr_target_integrity = np.array([0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.00000001, 0.000000001, 0.0000000001, 0.00000000001])
+# arr_target_integrity = np.array([0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.00000001, 0.000000001, 0.0000000001, 0.00000000001])
+arr_target_integrity_exp = np.arange(1, 20, step=1) # 1:1:20
 arr_sample_dof = np.arange(1, 20, step=1) # 1:1:20
 
 # init config
 config = configparser.ConfigParser()
 
-for integrityIdx in range(len(arr_target_integrity)):
+for integrityIdx in range(len(arr_target_integrity_exp)):
     integrity_dof_table_list = list()
     integrity_k_table_list = list()
 
-    # save in csv file
-    file_name_tir = str(arr_target_integrity[integrityIdx]) + '.ini'
+    target_integrity = 1 / 10**arr_target_integrity_exp[integrityIdx]
+    ini_target_integrity_exp = '1e-' + str(arr_target_integrity_exp[integrityIdx])
 
-    config.add_section(str(arr_target_integrity[integrityIdx]))
+    config.add_section(ini_target_integrity_exp)
 
     for dofIdx in range(len(arr_sample_dof)):
-        k = kCalculation(arr_sample_dof[dofIdx], d, arr_target_integrity[integrityIdx], 1.0)
+        k = kCalculation(arr_sample_dof[dofIdx], d, arr_target_integrity_exp[integrityIdx], 1.0)
         integrity_dof_table_list.append(arr_sample_dof[dofIdx])
         integrity_k_table_list.append(k[0])
 
     dof_str = ', '.join(map(str, integrity_dof_table_list))
-    config.set(str(arr_target_integrity[integrityIdx]), 'cfg_vec_dof', dof_str)
+    config.set(ini_target_integrity_exp, 'cfg_vec_dof', dof_str)
 
     k_str = ', '.join(map(str, integrity_k_table_list))
-    config.set(str(arr_target_integrity[integrityIdx]), 'cfg_vec_k', k_str)
+    config.set(ini_target_integrity_exp, 'cfg_vec_k', k_str)
 
     print(integrity_dof_table_list)
     print(integrity_k_table_list)
